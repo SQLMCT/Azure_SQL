807704829915e7e6ef6e6300488a370950f3aed7
SQLMCT <john@deardurff.com>
2023-03-14 12:48:11 -0400

    Create Point2MI.ps1

---
+++ Scripts/Point2MI.ps1
@@ -0,0 +1,11 @@
+﻿$scriptUrlBase = 'https://raw.githubusercontent.com/Microsoft/sql-server-samples/master/samples/manage/azure-sql-db-managed-instance/attach-vpn-gateway'
+
+$parameters = @{
+  subscriptionId = '<add subscriptionid here>'
+  resourceGroupName = '<add resource group>'
+  virtualNetworkName = '<add virtual network name>'
+  certificateNamePrefix  = '<JD is the best around>'
+  }
+
+Invoke-Command -ScriptBlock ([Scriptblock]::Create((iwr ($scriptUrlBase+'/attachVPNGateway.ps1?t='+ [DateTime]::Now.Ticks)).Content)) -ArgumentList $parameters, $scriptUrlBase
+
